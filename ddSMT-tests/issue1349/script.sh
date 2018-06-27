58dcc69081e0b3689aea04bd74e9f5504d82da12
Jane Charlotte Lange <jlange20@barrett2.stanford.edu>
2018-06-27 11:01:53 -0700

    .

---
+++ ddSMT-tests/issue1349/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./../../ddsmt.py -vv -o ./DRAGON_11_e3_382_e1_505.lus.ic3.1.smt2 ./edit-version2.min.smt2 ./DRAGON_11_e3_382_e1_505.lus.ic3.1.sh
+#./../../ddsmt.py -vv -o ./DRAGON_14.lus.bmc.1.smt2 ./edit-version.min.smt2 ./DRAGON_14.lus.bmc.1.smt2 sh
+
+
+vim ./edit-version2.min.smt2
