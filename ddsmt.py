58c433e02d59050f80376d4ce64901ec2a6d8655
janeclange <jlange20@stanford.edu>
2018-07-24 11:10:24 -0700

    Timeouts (#6)

--- ddsmt.py
+++ ddsmt.py
@@ -29,8 +29,7 @@ import shutil
 import time
 
 from argparse import ArgumentParser, REMAINDER
-from subprocess import Popen, PIPE
-from threading import Thread
+from subprocess import Popen, PIPE, TimeoutExpired
 from parser.ddsmtparser import DDSMTParser, DDSMTParseException
 
 
@@ -40,6 +39,8 @@ __author__  = "Aina Niemetz <aina.niemetz@gmail.com>"
 
 g_golden_exit = 0
 g_golden_err = None
+g_golden_runtime = 0
+g_current_runtime = 0
 g_ntests = 0
 g_testtime = 0
 g_args = None
@@ -57,30 +58,32 @@ class DDSMTException (Exception):
         return "[ddsmt] Error: {}".format(self.msg)
 
 
-class DDSMTCmd (Thread):
-
-    def __init__ (self, cmd, timeout, log):
-        Thread.__init__(self)
+class DDSMTCmd ():
+    def __init__(self, cmd, timeout, log):
         self.cmd = cmd
         self.timeout = timeout
         self.log = log
 
-    def run (self):
+    def run_cmd(self, is_golden = False):
+        global g_golden_runtime
         self.process = Popen (self.cmd, stdout=PIPE, stderr=PIPE)
-        self.out, self.err = self.process.communicate()
-        self.rcode = self.process.returncode
-
-    def run_cmd (self, is_golden = False):
-        self.start()
-        self.join (self.timeout)
-        if self.is_alive():
-            self.process.terminate()
-            self.join()
+        start = time.time()
+        try:
+            if is_golden:
+                self.out, self.err = self.process.communicate()
+                g_golden_runtime = time.time() - start
+                g_current_runtime = g_golden_runtime
+            else:
+                self.out, self.err = self.process.communicate(timeout=self.timeout)
+        except TimeoutExpired:
+            self.process.kill()
+            self.out, self.err = None, None
+            self.log (2, "[!!] timeout: process terminated")
             if is_golden:
                 raise DDSMTException ("initial run timed out")
-            self.log (2, "[!!] timeout: process terminated")
-        return (self.out, self.err)
 
+        self.rcode = self.process.returncode
+        return (self.out, self.err)
 
 def _cleanup ():
     if os.path.exists(g_tmpfile):
@@ -110,10 +113,16 @@ def _dump (filename = None, root = None):
 
 
 def _run (is_golden = False):
-    global g_args
+    global g_args, g_golden_runtime, g_current_runtime
     try:
-        start = time.time()
-        cmd = DDSMTCmd (g_args.cmd, g_args.timeout, _log)
+        if not g_args.timeout:
+            cmd = DDSMTCmd (g_args.cmd, g_golden_runtime, _log)
+        elif g_args.timeout_relative:
+            cmd = DDSMTCmd (g_args.cmd, g_args.timeout + g_golden_runtime, _log)
+        elif g_args.timeout_dynamic:
+            cmd = DDSMTCmd (g_args.cmd, g_args.timeout + g_current_runtime, _log)
+        else:
+            cmd = DDSMTCmd (g_args.cmd, g_args.timeout, _log)
         (out, err) = cmd.run_cmd(is_golden)
         return (cmd.rcode, err)
     except OSError as e:
@@ -146,7 +155,6 @@ def _filter_scopes (filter_fun, bfs, root = None):
        :roots:      List of nodes from which to begin searching.
        :bfs:        Bool indicating whether to use breadth-first search.
        :return:     List of scope nodes that fit the filtering condition.
-
     """
     global g_smtformula
     assert (g_smtformula)
@@ -205,7 +213,6 @@ def _filter_terms (filter_fun, bfs, roots):
        :bfs:        Bool indicating whether to use breadth-first search.
        :return:     List of term nodes that fit the filtering condition.
     """
-
     nodes = []
     to_visit = roots
     visited = {}
@@ -243,15 +250,15 @@ def _substitute (subst_fun, substlist, superset, randomized,  with_vars = False)
        :with_vars:  Bool indicating whether the substitution creates new variables. 
        :return:     Total number of nodes substituted.
     """
-
-    global g_smtformula
+    global g_smtformula, g_current_runtime
     assert (g_smtformula)
     assert (substlist in (g_smtformula.subst_scopes, g_smtformula.subst_cmds,
                           g_smtformula.subst_nodes))
     nsubst_total = 0
     gran = len(superset)
-
+    
     while gran > 0:
+        start_time = time.time()
         if randomized:
             subsets = [random.sample(superset, gran) for s in range(0, len(superset), gran)]
         else:
@@ -259,6 +266,10 @@ def _substitute (subst_fun, substlist, superset, randomized,  with_vars = False)
 
         tests_performed = 0
         for subset in subsets:
+            if g_args.roundtime:
+                if time.time() - start_time > g_args.roundtime:
+                    _log (2, "[!!] test round timeout: skipping to next granularity")
+                    break
             tests_performed += 1
             nsubst = 0
             cpy_substs = substlist.substs.copy()
@@ -271,8 +282,9 @@ def _substitute (subst_fun, substlist, superset, randomized,  with_vars = False)
                 continue
 
             _dump (g_tmpfile)
-
+            start = time.time()
             if _test():
+                g_current_runtime = time.time() - start
                 _dump (g_args.outfile)
                 nsubst_total += nsubst
                 _log (2, "    granularity: {}, subset {} of {}:, substituted: {}" \
@@ -709,10 +721,20 @@ if __name__ == "__main__":
                               default=False, help="randomize substitution subsets ")
         aparser.add_argument ("-b", action="store_true", dest="bfs",\
                               default=False, help="search for terms in breadth-first order ")
-        aparser.add_argument ("-t", dest="timeout", metavar="val",
-                              default=None, type=float,
-                              help="timeout for test runs in seconds "\
-                                   "(default: none)")
+        aparser.add_argument ("-t", dest="timeout", metavar="val",\
+                              default=None, type=float, \
+                              help="absolute: timeout for test runs in seconds " \
+                                   "relative: timeout is [val] seconds longer than golden runtime" \
+                                   "dynamic: timeout is [val] seconds longer than most recent successful test"\
+                                   "(default: absolute. When timeout is unspecified, default is golden runtime.)")
+        timeout_group = aparser.add_mutually_exclusive_group()
+        timeout_group.add_argument ("--rel", action="store_true", dest="timeout_relative",\
+                              default=False, help="timeouts are relative to test time of input file")
+        timeout_group.add_argument ("--dyn", action="store_true", dest="timeout_dynamic",\
+                              default=False, help="timeouts are relative to the runtime of the "\
+                                   "most recent successful test")
+        aparser.add_argument ("--round", dest="roundtime", metavar = "val", default=None,
+                              type=float, help="approximate time limit for testing rounds in seconds")
         aparser.add_argument ("-v", action="count", default=0,
                               dest="verbosity", help="increase verbosity")
         aparser.add_argument ("-o", action="store_false", dest="cmpoutput",
@@ -786,6 +808,7 @@ if __name__ == "__main__":
         if g_args.cmpoutput:
             _log (1, "golden err: {}".format(
                         str(g_golden_err.decode()).strip()))
+        _log (1, "golden runtime: {0: .2f} seconds".format(g_golden_runtime))
         ddsmt_main ()
 
         ofilesize = os.path.getsize(g_args.outfile)
