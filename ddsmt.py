abb5bed64850648da555044b2c94d5b0ca5b601a
Aina Niemetz <aina.niemetz@gmail.com>
2018-10-10 12:08:56 -0700

    Set default time limit to 1.5 * golden time.

--- ddsmt.py
+++ ddsmt.py
@@ -117,7 +117,7 @@ def _run (is_golden = False):
     global g_args, g_golden_runtime, g_current_runtime
     try:
         if not g_args.timeout:
-            cmd = DDSMTCmd(g_args.cmd, g_golden_runtime, _log)
+            cmd = DDSMTCmd(g_args.cmd, 1.5 * g_golden_runtime, _log)
         elif g_args.timeout_relative:
             cmd = DDSMTCmd(g_args.cmd, g_args.timeout + g_golden_runtime, _log)
         elif g_args.timeout_dynamic:
@@ -141,18 +141,18 @@ def _test ():
 
 def _filter_scopes (filter_fun, bfs, root = None):
     """_filter_scopes(filter_fun, bfs, root)
-   
-       Collect a list of scope nodes that fit a condition defined by given 
-       filtering function filter_fun. Nodes are added to the list in the order 
-       that they are visited in a depth-first search descending from a given 
+
+       Collect a list of scope nodes that fit a condition defined by given
+       filtering function filter_fun. Nodes are added to the list in the order
+       that they are visited in a depth-first search descending from a given
        root.
 
-       If no root is specified, search will begin from the root of the entire 
+       If no root is specified, search will begin from the root of the entire
        formula.
 
        If bfs is True, nodes are visited in a breadth-first search instead.
 
-       :filter_fun: Boolean function that returns True if a node should be 
+       :filter_fun: Boolean function that returns True if a node should be
                     added.
        :roots:      List of nodes from which to begin searching.
        :bfs:        Bool indicating whether to use breadth-first search.
@@ -177,13 +177,13 @@ def _filter_scopes (filter_fun, bfs, root = None):
 def _filter_cmds (filter_fun, bfs):
     """_filter_cmds(filter_fun, bfs)
 
-       Collect a list of command nodes that fit a condition defined by given 
+       Collect a list of command nodes that fit a condition defined by given
        filtering function filter_fun.
 
-       If bfs is True, scopes will be collected by breadth-first search instead 
+       If bfs is True, scopes will be collected by breadth-first search instead
        of depth-first.
 
-       :filter_fun: Boolean function that returns True if a node should be 
+       :filter_fun: Boolean function that returns True if a node should be
                     added.
        :bfs:        Bool indicating whether to use breadth-first search.
        :return:     List of command nodes that fit the filtering condition.
@@ -203,16 +203,16 @@ def _filter_cmds (filter_fun, bfs):
     return cmds
 
 def _filter_terms (filter_fun, bfs, roots):
-    """_filter_terms(filter_fun, bfs, roots) 
-       
-       Collect a list of term nodes that fit a condition defined by given 
-       filtering function filter_fun. Nodes are added to the list in the order 
-       that they are visited in a depth-first search descending from a given 
+    """_filter_terms(filter_fun, bfs, roots)
+
+       Collect a list of term nodes that fit a condition defined by given
+       filtering function filter_fun. Nodes are added to the list in the order
+       that they are visited in a depth-first search descending from a given
        list of roots.
 
        If bfs is True, nodes are visited in a breadth-first search instead.
 
-       :filter_fun: Boolean function that returns True if a node should be 
+       :filter_fun: Boolean function that returns True if a node should be
                     added.
        :roots:      List of nodes from which to begin searching.
        :bfs:        Bool indicating whether to use breadth-first search.
@@ -237,24 +237,24 @@ def _filter_terms (filter_fun, bfs, roots):
             to_visit.extend(cur.children)
     return nodes
 
-def _substitute (subst_fun, substlist, superset, randomized,  
+def _substitute (subst_fun, substlist, superset, randomized,
                  with_vars = False):
     """_substitute(subst_fun, substlist, superset, randomized, with_vars)
 
-       Attempt to substitute nodes in contiguous subsets as defined by given 
-       substitution function subst_fun. Remove substituted nodes from superset 
-       when substitution was successful. 
+       Attempt to substitute nodes in contiguous subsets as defined by given
+       substitution function subst_fun. Remove substituted nodes from superset
+       when substitution was successful.
 
-       If randomized is True, sample subsets randomly rather than splitting 
+       If randomized is True, sample subsets randomly rather than splitting
        into contiguous subsets.
 
        :subst_fun:  Function used to determine node substitutions.
-       :substlist:  Map from nodes in the input formula to their corresponding 
+       :substlist:  Map from nodes in the input formula to their corresponding
                     nodes in the reduced formula.
        :superset:   List of nodes to attempt to substitute.
        :randomized: Bool indicating whether to randomize subset selection.
-       :with_vars:  Bool indicating whether the substitution creates new 
-                    variables. 
+       :with_vars:  Bool indicating whether the substitution creates new
+                    variables.
        :return:     Total number of nodes substituted.
     """
     global g_smtformula, g_current_runtime
@@ -263,7 +263,7 @@ def _substitute (subst_fun, substlist, superset, randomized,
     assert (substlist is g_smtformula.substs)
     nsubst_total = 0
     gran = len(superset)
-    
+
     while gran > 0:
         start_time = time.time()
         if randomized:
@@ -296,12 +296,12 @@ def _substitute (subst_fun, substlist, superset, randomized,
                 _dump (g_args.outfile)
                 nsubst_total += nsubst
                 _log (2, "    granularity: {}, subset {} of {}, " \
-                         "substituted: {}".format(gran, tests_performed, 
+                         "substituted: {}".format(gran, tests_performed,
                       len(subsets), nsubst), True)
                 superset = list(set(superset) - set(subset))
             else:
                 _log (2, "    granularity: {}, subset {} of {}, "\
-                         "substituted: 0".format(gran, tests_performed, 
+                         "substituted: 0".format(gran, tests_performed,
                       len(subsets)), True)
                 substlist.substs = cpy_substs
                 if with_vars:
@@ -318,14 +318,14 @@ def _substitute (subst_fun, substlist, superset, randomized,
 def _substitute_scopes (bfs, randomized):
     """_substitute_scopes(bfs, randomized)
 
-       Attempt to remove scope nodes at each level of the formula by 
-       substituting them with None. 
+       Attempt to remove scope nodes at each level of the formula by
+       substituting them with None.
 
-       :bfs:        Bool indicating whether to collect nodes in breadth-first 
+       :bfs:        Bool indicating whether to collect nodes in breadth-first
                     order.
        :randomized: Bool indicating whether to randomize subset selection for
                     substitution.
-       :return:     Total number of nodes substituted. 
+       :return:     Total number of nodes substituted.
     """
     global g_smtformula
     assert (g_smtformula)
@@ -335,7 +335,7 @@ def _substitute_scopes (bfs, randomized):
     nsubst_total = 0
     level = 1
     while True:
-        scopes = _filter_scopes (lambda x: x.level == level and 
+        scopes = _filter_scopes (lambda x: x.level == level and
                                  x.is_regular(), bfs)
         if not scopes:
             break
@@ -349,14 +349,14 @@ def _substitute_scopes (bfs, randomized):
 def _substitute_cmds (bfs, randomized, filter_fun = None):
     """_substitute_cmds(filter_fun, bfs, randomized)
 
-       Attempt to remove command nodes as defined by a given filtering function 
-       filter_fun by substituting them with None. 
+       Attempt to remove command nodes as defined by a given filtering function
+       filter_fun by substituting them with None.
 
-       :bfs:        Bool indicating whether to collect nodes in breadth-first 
+       :bfs:        Bool indicating whether to collect nodes in breadth-first
                     order.
        :randomized: Bool indicating whether to randomize subset selection for
                     substitution.
-       :return:     Total number of nodes substituted. 
+       :return:     Total number of nodes substituted.
     """
     global g_smtformula
     assert (g_smtformula)
@@ -374,10 +374,10 @@ def _substitute_cmds (bfs, randomized, filter_fun = None):
 
 def _substitute_terms (subst_fun, filter_fun, cmds, bfs, randomized, msg = None,
                        with_vars = False):
-    """_substitute_terms(subst_fun, filter_fun, cmds, bfs, randomized, msg, 
+    """_substitute_terms(subst_fun, filter_fun, cmds, bfs, randomized, msg,
                          with_vars)
 
-       Attempt to substitute term nodes as defined by given substitution 
+       Attempt to substitute term nodes as defined by given substitution
        function subst_fun and filtering condition filter_fun. Terms descend from
        a given command list cmds and are collected in the order indicated by the
        bfs parameter.
@@ -385,13 +385,13 @@ def _substitute_terms (subst_fun, filter_fun, cmds, bfs, randomized, msg = None,
        :subst_fun:  Function used to determine node substitutions.
        :filter_fun: Function used to select terms to substitute.
        :cmds:       List of commands to substitute terms from.
-       :bfs:        Bool indicating whether to collect nodes in breadth-first 
+       :bfs:        Bool indicating whether to collect nodes in breadth-first
                     order.
        :randomized: Bool indicating whether to randomize subset selection.
        :msg:        String to write to the log.
-       :with_vars:  Bool indicating whether the substitution creates new 
-                    variables. 
-       :return:     Total number of nodes substituted. 
+       :with_vars:  Bool indicating whether the substitution creates new
+                    variables.
+       :return:     Total number of nodes substituted.
     """
     _log (2)
     _log (2, msg if msg else "substitute TERMS:")
@@ -441,7 +441,7 @@ def ddsmt_main ():
         if nrounds > 1:
            nsubst = _substitute_cmds (g_args.bfs, g_args.randomized)
         else:
-           nsubst = _substitute_cmds (g_args.bfs, g_args.randomized, 
+           nsubst = _substitute_cmds (g_args.bfs, g_args.randomized,
                                       lambda x: x.is_assert())
         if nsubst:
            succeeded = "cmds"
@@ -465,7 +465,7 @@ def ddsmt_main ():
                             lambda x: sf.bvZeroConstNode(x.sort),
                             lambda x: not x.is_const() \
                                       and x.sort and x.sort.is_bv_sort(),
-                            cmds[i], g_args.bfs, g_args.randomized, 
+                            cmds[i], g_args.bfs, g_args.randomized,
                             "  substitute BV terms with '0'")
                     if nsubst:
                         succeeded = "bv0_{}".format(i)
@@ -481,7 +481,7 @@ def ddsmt_main ():
                                 (x.children[0].get_subst().is_false_bvconst() \
                                  or
                                  x.children[1].get_subst().is_false_bvconst()),
-                            cmds[i], g_args.bfs, g_args.randomized, 
+                            cmds[i], g_args.bfs, g_args.randomized,
                             "  substitute (bvor term false) with term")
                     if nsubst:
                         succeeded = "bvor_{}".format(i)
@@ -497,7 +497,7 @@ def ddsmt_main ():
                                 (x.children[0].get_subst().is_true_bvconst() \
                                  or
                                  x.children[1].get_subst().is_true_bvconst()),
-                            cmds[i], g_args.bfs, g_args.randomized, 
+                            cmds[i], g_args.bfs, g_args.randomized,
                             "  substitute (bvand term true) with term")
                     if nsubst:
                         succeeded = "bvand_{}".format(i)
@@ -525,7 +525,7 @@ def ddsmt_main ():
                             lambda x: sf.zeroConstNNode(),
                             lambda x: not x.is_const() \
                                       and x.sort and x.sort.is_int_sort(),
-                            cmds[i], g_args.bfs, g_args.randomized, 
+                            cmds[i], g_args.bfs, g_args.randomized,
                             "  substitute Int terms with '0'")
                     if nsubst:
                         succeeded = "int0_{}".format(i)
@@ -553,7 +553,7 @@ def ddsmt_main ():
                             lambda x: sf.zeroConstDNode(),
                             lambda x: not x.is_const() \
                                       and x.sort and x.sort.is_real_sort(),
-                            cmds[i], g_args.bfs, g_args.randomized, 
+                            cmds[i], g_args.bfs, g_args.randomized,
                             "  substitute Real terms with '0'")
                     if nsubst:
                         succeeded = "real0_{}".format(i)
@@ -579,7 +579,7 @@ def ddsmt_main ():
                 nsubst = _substitute_terms (
                         lambda x: x.children[-1].get_subst(),
                         lambda x: x.is_let(),
-                        cmds[i], g_args.bfs, g_args.randomized, 
+                        cmds[i], g_args.bfs, g_args.randomized,
                         "  substitute LETs with child term")
                 if nsubst:
                     succeeded = "let_{}".format(i)
@@ -604,7 +604,7 @@ def ddsmt_main ():
                         lambda x: sf.boolConstNode("false"),
                         lambda x: not x.is_const() \
                                   and x.sort and x.sort.is_bool_sort(),
-                        cmds[i], g_args.bfs, g_args.randomized, 
+                        cmds[i], g_args.bfs, g_args.randomized,
                         "  substitute Boolean terms with 'false'")
                 if nsubst:
                     succeeded = "false_{}".format(i)
@@ -620,7 +620,7 @@ def ddsmt_main ():
                         lambda x: x.is_or() \
                                 and (x.children[0].get_subst().is_false_const()\
                                 or x.children[1].get_subst().is_false_const()),
-                        cmds[i], g_args.bfs, g_args.randomized, 
+                        cmds[i], g_args.bfs, g_args.randomized,
                         "  substitute (or term false) with term")
                 if nsubst:
                     succeeded = "or_{}".format(i)
@@ -633,7 +633,7 @@ def ddsmt_main ():
                         lambda x: sf.boolConstNode("true"),
                         lambda x: not x.is_const() \
                                   and x.sort and x.sort.is_bool_sort(),
-                        cmds[i], g_args.bfs, g_args.randomized, 
+                        cmds[i], g_args.bfs, g_args.randomized,
                         "  substitute Boolean terms with 'true'")
                 if nsubst:
                     succeeded = "true_{}".format(i)
@@ -735,11 +735,11 @@ if __name__ == "__main__":
         aparser.add_argument ("cmd", nargs=REMAINDER,
                               help="the command (with optional arguments)")
 
-        aparser.add_argument ("-r", action="store_true", dest="randomized", 
-                              default=False, 
+        aparser.add_argument ("-r", action="store_true", dest="randomized",
+                              default=False,
                               help="randomize substitution subsets")
         aparser.add_argument ("-b", action="store_true", dest="bfs",\
-                              default=False, 
+                              default=False,
                               help="search for terms in breadth-first order ")
         aparser.add_argument ("-t", dest="timeout", metavar="val",\
                               default=None, type=float, \
@@ -749,17 +749,18 @@ if __name__ == "__main__":
                                    "dynamic: timeout is [val] seconds longer "\
                                    "than most recent successful test. "\
                                    "(default: absolute. When timeout is "\
-                                   "unspecified, default is golden runtime.)")
+                                   "unspecified, default is " \
+                                   "1.5 * golden runtime.)")
         timeout_group = aparser.add_mutually_exclusive_group()
-        timeout_group.add_argument ("--rel", action="store_true", 
+        timeout_group.add_argument ("--rel", action="store_true",
                               dest="timeout_relative", default=False, \
                               help="timeouts are relative to \
                                     test time of input file")
-        timeout_group.add_argument ("--dyn", action="store_true", 
-                              dest="timeout_dynamic", default=False, 
+        timeout_group.add_argument ("--dyn", action="store_true",
+                              dest="timeout_dynamic", default=False,
                               help="timeouts are relative to the runtime of"\
                                    "the most recent successful test")
-        aparser.add_argument ("--round", dest="roundtime", metavar = "val", 
+        aparser.add_argument ("--round", dest="roundtime", metavar = "val",
                               default=None, type=float, help="approximate time"\
                                    " limit for testing rounds in seconds")
         aparser.add_argument ("-v", action="count", default=0,
@@ -767,7 +768,7 @@ if __name__ == "__main__":
         aparser.add_argument ("-o", dest="cmpoutput",
                               help = "use exit code and search pattern string "\
                                      "to identify failing input (default: "\
-                                     "error exit code and stderr output)") 
+                                     "error exit code and stderr output)")
         aparser.add_argument ("--version", action="version",
                               version=__version__)
         g_args = aparser.parse_args()
