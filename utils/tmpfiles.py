dc6c22e726b6768caf6a8466e7ceb9f0818c8f67
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-12-10 22:57:22 +0100

    Work.

--- utils/tmpfiles.py
+++ utils/tmpfiles.py
@@ -4,12 +4,16 @@ import tempfile
 
 from utils import options
 
+# The temporary directory. Automatically deleted upon termination.
 __TMPDIR = tempfile.TemporaryDirectory(prefix="ddsmt-")
 __BINARY = os.path.join(__TMPDIR.name, 'binary')
 __BINARY_CC = os.path.join(__TMPDIR.name, 'binary_cc')
 
 
 def copy_binaries():
+    """Copy the solver binary from :code:`cmd` to our temporary directory.
+    If a cross check is defined, also copies this binary. After copying, it
+    modifies the :code:`cmd` argument to use the copied binary."""
     shutil.copy(options.args().cmd[0], __BINARY)
     options.args().cmd[0] = __BINARY
 
@@ -19,10 +23,13 @@ def copy_binaries():
 
 
 def get_tmp_filename():
-    return tempfile.TemporaryFile(dir=__TMPDIR.name)
+    """Return a filename within our temporary directory based on the pid."""
+    return os.path.join(__TMPDIR.name, "ddsmt-tmp-{}.smt2".format(os.getpid()))
 
 
 def copy_to_tmp_file(source):
+    """Copy the given source file to a temporary file as returned by
+    :code:`get_tmp_filename()`."""
     res = get_tmp_filename()
     shutil.copy(source, res.name)
     return res
