c8176341148ba4b23ec33a318b43c0cc12543341
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-05 10:29:58 +0100

    Reorganize options, add --memout.

--- utils/options.py
+++ utils/options.py
@@ -20,17 +20,16 @@ def parse_options():
                     default=os.cpu_count(),
                     help="use nprocs parallel processes, default: {}".format(
                         os.cpu_count()))
-    ap.add_argument("-c", dest="cmd_cc", help="cross check command")
     ap.add_argument("-t",
                     dest="timeout",
                     type=float,
                     help="timeout for test runs in seconds, "\
                          "default: 1.5 * golden runtime")
-    ap.add_argument("--timeout-cc",
-                    dest="timeout_cc",
-                    type=float,
-                    help="timeout for test runs of the cross check in seconds, "\
-                         "default: 1.5 * golden runtime")
+    ap.add_argument('--memout',
+                    type=int,
+                    metavar='megabytes',
+                    default=0,
+                    help='memout for individual checks')
     ap.add_argument("-v",
                     action="count",
                     dest="verbosity",
@@ -40,23 +39,36 @@ def parse_options():
                     dest="match_err",
                     help="match string in stderr to identify "\
                          "failing input (default: stderr output)")
-    ap.add_argument("--match-err-cc",
-                    dest="match_err_cc",
-                    help="match string to identify failing input for "\
-                         "cross check command (default: stderr output)")
     ap.add_argument("--match-out",
                     dest="match_out",
                     help="match string in stdout to identify "\
                          "failing input (default: stdout output)")
-    ap.add_argument("--match-out-cc",
-                    dest="match_out_cc",
-                    help="match string to identify failing input "
-                    "for cross check command (default: stdout output)")
     ap.add_argument("--parser-test",
                     action="store_true",
                     dest="parser_test",
                     help="run ddSMT in parser test mode "\
                          "(parses only, does not require command argument)")
+
+    apcc = ap.add_argument_group('cross check')
+    apcc.add_argument("-c",
+                      metavar='cmd-cc',
+                      dest="cmd_cc",
+                      help="cross check command")
+    apcc.add_argument("--timeout-cc",
+                    dest="timeout_cc",
+                    metavar='timeout',
+                    type=float,
+                    help="timeout for test runs of the cross check in seconds, "\
+                         "default: 1.5 * golden runtime")
+    apcc.add_argument("--match-err-cc",
+                    dest="match_err_cc",
+                    help="match string to identify failing input for "\
+                         "cross check command (default: stderr output)")
+    apcc.add_argument("--match-out-cc",
+                      dest="match_out_cc",
+                      help="match string to identify failing input "
+                      "for cross check command (default: stdout output)")
+
     res = ap.parse_args()
 
     if res.cmd_cc:
