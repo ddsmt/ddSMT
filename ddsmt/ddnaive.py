91d2dfc9e866618cef7461b8ce61ec78d5f354c2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-08 21:03:30 +0100

    Adapt ddnaive for Nodes

--- ddsmt/ddnaive.py
+++ ddsmt/ddnaive.py
@@ -5,6 +5,7 @@ import sys
 import time
 
 from . import checker
+from . import nodes
 from . import options
 from . import parser
 from . import subst
@@ -42,7 +43,7 @@ class MutationGenerator:
                         map(
                             lambda x: Mutation(
                                 self.__node_count, str(m),
-                                subst.subs_local(ginput, linput, x)),
+                                nodes.substitute(ginput, {linput.id: x})),
                             m.mutations(linput)))
                 if hasattr(m, 'global_mutations'):
                     yield from list(
@@ -57,7 +58,7 @@ class MutationGenerator:
     def generate_mutations(self, original, skip):
         """A generator that produces all possible mutations from the given
         original."""
-        for node in smtlib.dfs(original):
+        for node in nodes.dfs(original):
             self.__node_count += 1
             if skip < self.__node_count:
                 for task in self.__mutate_node(node, original):
