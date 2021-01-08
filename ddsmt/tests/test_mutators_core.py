c59140e2874aa4a63dd11cd9504d7ee59e268de2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-08 19:38:04 +0100

    More tests.

---
+++ ddsmt/tests/test_mutators_core.py
@@ -0,0 +1,24 @@
+from ..nodes import Node
+from .. import mutators_core
+from .. import smtlib
+
+
+def test_constants():
+    exprs = [Node(Node('declare-const'), Node('x'), Node('Real'))]
+    smtlib.collect_information(exprs)
+    m = mutators_core.Constants()
+    assert m.mutations(Node('x')) == [Node('0.0'), Node('1.0')]
+
+
+def test_erase_node():
+    m = mutators_core.EraseNode()
+    assert m.mutations(Node('x')) == [None]
+
+
+def test_merge_with_children():
+    node = Node(Node('+'), Node('x'), Node(Node('+'), Node('x'), Node('y')))
+    m = mutators_core.MergeWithChildren()
+    assert m.filter(node)
+    assert m.mutations(node) == [
+        Node(Node('+'), Node('x'), Node('x'), Node('y'))
+    ]
