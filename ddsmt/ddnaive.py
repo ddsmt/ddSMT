c9dc0c16385fdf9ad725a2a66c0c16bc6171d212
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-13 10:33:44 +0100

    Working on ddnaive, hopefully avoiding deadlocks now.

--- ddsmt/ddnaive.py
+++ ddsmt/ddnaive.py
@@ -1,6 +1,6 @@
 import collections
 import logging
-from multiprocessing import Pool
+import multiprocessing
 import pickle
 import sys
 import time
@@ -11,16 +11,6 @@ from . import nodes
 from . import options
 from . import progress
 from . import smtlib
-
-# nodeid: id of the mutated node in dfs order. Only used for progress indication
-# name: name of the mutator
-# if we have a global mutation:
-#   exprs: None
-#   repl: the new input to check
-# if we have a local mutation:
-#   exprs: the current input
-#   repl: the substitution to be checked
-Task = collections.namedtuple('Task', ['nodeid', 'name', 'exprs', 'repl'])
 
 
 def ddnaive_passes():
@@ -81,21 +71,40 @@ def ddnaive_passes():
     ]
 
 
-class MutationGenerator:
-    def __init__(self, skip, mutators):
+# nodeid: id of the mutated node in dfs order. Only used for progress indication
+# name: name of the mutator
+# if we have a global mutation:
+#   exprs: None
+#   repl: the new input to check
+# if we have a local mutation:
+#   exprs: the current input
+#   repl: the substitution to be checked
+Task = collections.namedtuple('Task', ['nodeid', 'name', 'exprs', 'repl'])
+
+
+class Producer:
+    """Manages the generation of candidates that shall be checked.
+
+    Performs a walk through the current input and applies the
+    :code:`mutators` to every node. Supports skipping the first
+    :code:`skip` nodes. As soon as :code:`abort_flag` is triggered,
+    stops generation as soon as possible.
+    """
+    def __init__(self, skip, mutators, abort_flag):
         self.__node_count = 0
         self.__node_skip = skip
         self.__mutators = mutators
+        self.__abort = abort_flag
 
     def __mutate_node(self, linput, ginput, gpickled):
-        """Apply all active mutators to the given node.
+        """Apply all mutators to the given node.
 
-        Returns a list of all possible mutations as tuples :code:`(name,
-        local, global)` where :code:`local` is a modification of the
-        current node and :code:`global` is a modification of the whole
-        input and one of those is always :code:`None`.
+        Returns a list of all possible mutations as :code:`Task`
+        objects.
         """
         for m in self.__mutators:
+            if self.__abort.is_set():
+                break
             try:
                 if hasattr(m, 'filter') and not m.filter(linput):
                     continue
@@ -114,33 +123,49 @@ class MutationGenerator:
             except Exception as e:
                 logging.info(f'{type(e)} in application of {m}: {e}')
 
-    def generate_mutations(self, original):
-        """A generator that produces all possible mutations from the given
-        original."""
+    def generate(self, original):
+        """A generator that produces all possible mutations as :code:`Task`
+        from the given original."""
         pickled = pickle.dumps(original)
         for node in nodes.dfs(original):
             self.__node_count += 1
             if self.__node_skip < self.__node_count:
                 yield from self.__mutate_node(node, original, pickled)
+            if self.__abort.is_set():
+                break
 
 
-def _check(task):
-    try:
-        if task.exprs is None:
-            # global
-            exprs = pickle.loads(task.repl)
-        else:
-            # local
-            exprs = nodes.substitute(pickle.loads(task.exprs),
-                                     pickle.loads(task.repl))
-        if checker.check_exprs(exprs):
-            return True, Task(task.nodeid, task.name, exprs, None)
-        return False, Task(task.nodeid, task.name, None, None)
-    except Exception as e:
-        logging.info(f'{type(e)} in check of {task.name}: {e}')
+class Consumer:
+    """Calls the :code:`checker` on individual tasks to figure out whether they
+    are valid simplifications.
+
+    Uses the :code:`abort_flag` to stop as soon as a valid
+    simplification has been found.
+    """
+    def __init__(self, abort_flag):
+        self.__abort = abort_flag
+
+    def check(self, task):
+        if self.__abort.is_set():
+            return False, Task(task.nodeid, task.name, None, None)
+        try:
+            if task.exprs is None:
+                # global
+                exprs = pickle.loads(task.repl)
+            else:
+                # local
+                exprs = nodes.substitute(pickle.loads(task.exprs),
+                                         pickle.loads(task.repl))
+            if checker.check_exprs(exprs):
+                return True, Task(task.nodeid, task.name, exprs, None)
+            return False, Task(task.nodeid, task.name, None, None)
+        except Exception as e:
+            logging.info(f'{type(e)} in check of {task.name}: {e}')
 
 
 def reduce(exprs):
+    """Reduces the input given in :code:`exprs` as good as possible in a fixed-
+    point loop."""
     passes = ddnaive_passes()
     cur_pool = 1
     cur_passes = passes[0]
@@ -157,14 +182,21 @@ def reduce(exprs):
             cnt = smtlib.count_nodes(exprs)
             progress.start(cnt)
             progress.update(min(cnt, skip))
-            with Pool(options.args().max_threads) as pool:
-                mg = MutationGenerator(skip, cur_passes)
-                for result in pool.imap_unordered(
-                        _check, mg.generate_mutations(exprs)):
+
+            with multiprocessing.Pool(options.args().max_threads) as pool:
+                # multiprocessing: abort flag is passed to both producer and consumer
+                abort_flag = multiprocessing.Manager().Event()
+                prod = Producer(skip, cur_passes, abort_flag)
+                cons = Consumer(abort_flag)
+                for result in pool.imap_unordered(cons.check,
+                                                  prod.generate(exprs)):
                     nchecks += 1
                     success, task = result
                     progress.update(task.nodeid)
                     if success:
+                        # trigger abort and close the pool, then process the result
+                        abort_flag.set()
+                        pool.close()
                         sys.stdout.write('\n')
                         runtime = time.time() - start
                         logging.info(
@@ -176,9 +208,9 @@ def reduce(exprs):
                         fresh_run = False
                         nodes.write_smtlib_to_file(options.args().outfile,
                                                    exprs)
-                        pool.close()
+                        # and finally join
+                        pool.join()
                         break
-            pool.join()
             if not reduction:
                 sys.stdout.write('\n')
                 logging.info('No further simplification found')
