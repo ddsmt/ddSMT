71e4a3625b1107009ec0b8188c4d780e8d5d207d
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-14 20:57:49 +0100

    Turn mutations into a list, just in case the mutator is a generator.

--- ddsmt/ddmin.py
+++ ddsmt/ddmin.py
@@ -84,7 +84,7 @@ def _subst(exprs, subset, mutator):
     else:
         substs = dict()
         for node in subset:
-            mutations = mutator.mutations(node)
+            mutations = list(mutator.mutations(node))
             substs[node.id] = mutations[0] if mutations else None
         res.append(nodes.substitute(exprs, substs))
 
