c48e116a724bebd37b8c634cc667a921a37e0c96
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-20 10:18:44 +0100

    Improve result_differs.py

--- scripts/result_differs.py
+++ scripts/result_differs.py
@@ -14,33 +14,24 @@ signal.signal(signal.SIGTERM, handler)
 
 
 def run(cmd):
-    return subprocess.run(cmd + [sys.argv[1]],
-                          capture_output=True).stdout.decode('utf8').strip()
+    cmd = cmd + [sys.argv[1]]
+    res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return res.stdout.decode('utf8').strip()
 
 
 A = run(['solverA', '--option'])
 B = run(['solverB', '--option'])
 
-if A == 'sat':
-    if B == 'sat':
-        print('sat / sat')
-        sys.exit(0)
-    elif B == 'unsat':
-        print('sat / unsat')
-        sys.exit(1)
-    else:
-        print('Unexpected output for B: {}'.format(B))
-        sys.exit(2)
-elif A == 'unsat':
-    if B == 'sat':
-        print('unsat / sat')
-        sys.exit(1)
-    elif B == 'unsat':
-        print('unsat / unsat')
-        sys.exit(0)
-    else:
-        print('Unexpected output for B: {}'.format(B))
-        sys.exit(2)
-else:
-    print('Unexpected output for A: {}'.format(A))
+if A not in ['sat', 'unsat']:
+    print(f'Unexpected output for A: {A}')
     sys.exit(2)
+
+if B not in ['sat', 'unsat']:
+    print(f'Unexpected output for B: {B}')
+    sys.exit(2)
+
+print(f'{A} / {B}')
+if A == B:
+    sys.exit(0)
+else:
+    sys.exit(1)
