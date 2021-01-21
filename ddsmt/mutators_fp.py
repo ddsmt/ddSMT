372e310ebf16bd712742663fc07fe080ee357b5e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-01-21 08:21:37 +0100

    Fix docstring for FPShortSort.

--- ddsmt/mutators_fp.py
+++ ddsmt/mutators_fp.py
@@ -1,23 +1,29 @@
 from .nodes import Node
-from . import options
 from .smtlib import *
 
 
 class FPShortSort:
     """Replace long sort names with short names.
 
-    | ``(_ FloatingPoint 5 11)``   --> ``Float16`` | ``(_ FloatingPoint
-    8 24)``   --> ``Float32`` | ``(_ FloatingPoint 11 53)``  -->
-    ``Float64`` | ``(_ FloatingPoint 15 113)`` --> ``Float128``
+    .. code-block:: common-lisp
+
+        (_ FloatingPoint 5 11)   --> Float16
+        (_ FloatingPoint 8 24)   --> Float32
+        (_ FloatingPoint 11 53)  --> Float64
+        (_ FloatingPoint 15 113) --> Float128
     """
     def filter(self, node):
         return is_fp_sort(node) and len(node) == 4
 
     def mutations(self, node):
-        if node[2] == '5' and node[3] == '11': return [Node('Float16')]
-        if node[2] == '8' and node[3] == '24': return [Node('Float32')]
-        if node[2] == '11' and node[3] == '53': return [Node('Float64')]
-        if node[2] == '15' and node[3] == '113': return [Node('Float128')]
+        if node[2] == '5' and node[3] == '11':
+            return [Node('Float16')]
+        if node[2] == '8' and node[3] == '24':
+            return [Node('Float32')]
+        if node[2] == '11' and node[3] == '53':
+            return [Node('Float64')]
+        if node[2] == '15' and node[3] == '113':
+            return [Node('Float128')]
         return []
 
     def __str__(self):
