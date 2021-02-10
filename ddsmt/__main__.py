b87d6b62b20631b857d34d7f8d2f79ba88fbdf94
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2021-02-10 17:44:32 +0100

    Remove now obsolete ddsmt-profile

--- ddsmt/__main__.py
+++ ddsmt/__main__.py
@@ -23,24 +23,5 @@ def main():
     return 1
 
 
-def main_profile():
-    """Wrapper for `main()` that enabled profiling."""
-    import cProfile
-    import subprocess
-    with cProfile.Profile() as pr:
-        pr.enable()
-        res = main()
-        pr.disable()
-        pr.dump_stats('profile.prof')
-    subprocess.run([
-        'gprof2dot', '--node-label=self-time-percentage',
-        '--node-label=total-time', '--node-label=total-time-percentage', '-f',
-        'pstats', '-o', 'profile.dot', 'profile.prof'
-    ])
-    subprocess.run(['dot', '-Tpng', '-o', 'profile.png', 'profile.dot'])
-    print('profile information is stored at profile.(prof|dot|png)')
-    return res
-
-
 if __name__ == '__main__':
     sys.exit(main())
