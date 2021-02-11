a92f7de65406d0fceefa975456ba6a9ab34a32f9
Mathias Preiner <mathias.preiner@gmail.com>
2021-02-11 10:26:40 -0800

    ddmin: Delay collect_information call.

--- ddsmt/strategy_ddmin.py
+++ ddsmt/strategy_ddmin.py
@@ -324,7 +324,6 @@ def _check_par(taskgen, nexprs, stats):
                     taskgen.stop()
                     taskgen.update(result.exprs)
                     nodeio.write_smtlib_to_file(outfile, taskgen.exprs)
-                    smtlib.collect_information(taskgen.exprs)
                     stats['tests_success'] += 1
                     stats['reduced'] += result.reduced
                     start_index = result.task_id + 1
@@ -347,6 +346,7 @@ def _check_par(taskgen, nexprs, stats):
 
             if __abort_flag.is_set():
                 __abort_flag.clear()
+                smtlib.collect_information(taskgen.exprs)
                 taskgen.reset(start_index)
                 taskgen.start()
                 logging.debug(f'Restart tests starting from {start_index}')
